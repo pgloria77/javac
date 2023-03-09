23d4b1f983a61ac6116cfbe51fd42190d3d83f89
pgloria77 <127390270+pgloria77@users.noreply.github.com>
2023-03-09 14:50:55 +0530

    Add files via upload

---
+++ pn.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class pn
+{
+ public static void main(String [] args)
+ {
+  Scanner s=new Scanner(System.in);
+  int n=s.nextInt();
+  if(n>=0)
+  {
+    System.out.println(+n+"is positive");
+  }
+  else
+  {
+   System.out.println(+n+"is negative");
+  }
+ }
+}
+   
